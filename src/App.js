a46e6b6c726d06cf32b9f8603435c99e8fbd5f6e
alexander <sanjinoridze@gmail.com>
2023-02-15 18:30:19 +0400

    Add fixes of search field or select btn focus

--- src/App.js
+++ src/App.js
@@ -1,61 +1,61 @@
 import { useState, useEffect } from "react";
 import Select from "./components/Select";
 
+const fruits = {
+    grapes: "🍇 Grapes",
+    melon: "🍈 Melon",
+    watermelon: "🍉 Watermelon",
+    tangerine: "🍊 Tangerine",
+    lemon: "🍋 Lemon",
+    banana: "🍌 Banana",
+    pineapple: "🍍 Pineapple",
+    mango: "🥭 Mango",
+    apple: "🍎 Apple",
+    pear: "🍐 Pear",
+    peach: "🍑 Peach",
+    strawberry: "🍓 Strawberry",
+};
+
+const vegies = {
+    tomato: "🍅 Tomato",
+    avocado: "🥑 Avocado",
+    eggplant: "🍆 Eggplant",
+    potato: "🥔 Potato",
+    carrot: "🥕 Carrot",
+    corn: "🌽 Ear of Corn",
+    hotPepper: "🌶️ Hot Pepper",
+    cucumber: "🥒 Cucumber",
+    leafyGreen: "🥬 Leafy Green",
+    broccoli: "🥦 Broccoli",
+    garlic: "🧄 Garlic",
+    onion: "🧅 Onion",
+};
+
+const breadNMeat = {
+    bread: "🍞 Bread",
+    croissant: "🥐 Croissant",
+    baguette: "🥖 Baguette Bread",
+    pretzel: "🥨 Pretzel",
+    bagel: "🥯 Bagel",
+    waffle: "🧇 Waffle",
+    meatOnBone: "🍖 Meat on Bone",
+    poultryLeg: "🍗 Poultry Leg",
+    cutOfMeat: "🥩 Cut of Meat",
+    bacon: "🥓 Bacon",
+    hamburger: "🍔 Hamburger",
+    hotdog: "🌭 Hot Dog",
+};
+
 function App() {
     const [selectedFruits, setSelectedFruits] = useState("");
     const [selectedVegies, setSelectedVegies] = useState([]);
     const [selectedBreadNMeat, setSelectedBreadNMeat] = useState("");
 
     useEffect(() => {
-        console.log('selectedFruits', selectedFruits);
-        console.log('selectedVegies', selectedVegies);
-        console.log('selectedBreadNMeat', selectedBreadNMeat);
+        console.log("selectedFruits", selectedFruits);
+        console.log("selectedVegies", selectedVegies);
+        console.log("selectedBreadNMeat", selectedBreadNMeat);
     }, [selectedFruits, selectedVegies, selectedBreadNMeat]);
-
-    const fruits = {
-        grapes: "🍇 Grapes",
-        melon: "🍈 Melon",
-        watermelon: "🍉 Watermelon",
-        tangerine: "🍊 Tangerine",
-        lemon: "🍋 Lemon",
-        banana: "🍌 Banana",
-        pineapple: "🍍 Pineapple",
-        mango: "🥭 Mango",
-        apple: "🍎 Apple",
-        pear: "🍐 Pear",
-        peach: "🍑 Peach",
-        strawberry: "🍓 Strawberry"
-    }
-
-    const vegies = {
-        tomato: "🍅 Tomato",
-        avocado: "🥑 Avocado",
-        eggplant: "🍆 Eggplant",
-        potato: "🥔 Potato",
-        carrot: "🥕 Carrot",
-        corn: "🌽 Ear of Corn",
-        hotPepper: "🌶️ Hot Pepper",
-        cucumber: "🥒 Cucumber",
-        leafyGreen: "🥬 Leafy Green",
-        broccoli: "🥦 Broccoli",
-        garlic: "🧄 Garlic",
-        onion: "🧅 Onion"
-    }
-
-    const breadNMeat = {
-        bread: "🍞 Bread",
-        croissant: "🥐 Croissant",
-        baguette: "🥖 Baguette Bread",
-        pretzel: "🥨 Pretzel",
-        bagel: "🥯 Bagel",
-        waffle: "🧇 Waffle",
-        meatOnBone: "🍖 Meat on Bone",
-        poultryLeg: "🍗 Poultry Leg",
-        cutOfMeat: "🥩 Cut of Meat",
-        bacon: "🥓 Bacon",
-        hamburger: "🍔 Hamburger",
-        hotdog: "🌭 Hot Dog"
-    }
 
     return (
         <div className="App">
@@ -65,7 +65,7 @@ function App() {
                 <Select
                     options={fruits}
                     value={selectedFruits}
-                    onChange={value => setSelectedFruits(value)}
+                    onChange={(value) => setSelectedFruits(value)}
                     placeholder="Select Fruits"
                 />
             </div>
@@ -74,13 +74,13 @@ function App() {
                 <Select
                     options={vegies}
                     value={selectedVegies}
-                    onChange={value => setSelectedVegies(value)}
-                    customOption={(value, label) =>
+                    onChange={(value) => setSelectedVegies(value)}
+                    customOption={(value, label) => (
                         <div>
                             <pre>{value}</pre>
                             <strong>{label}</strong>
                         </div>
-                    }
+                    )}
                     placeholder="Select Vegies"
                     withCheck="multiple"
                     withSearch
@@ -91,7 +91,7 @@ function App() {
                 <Select
                     options={breadNMeat}
                     value={selectedBreadNMeat}
-                    onChange={value => setSelectedBreadNMeat(value)}
+                    onChange={(value) => setSelectedBreadNMeat(value)}
                     placeholder="Select Meat and Bread"
                     withCheck="single"
                 />
--- src/components/Select.js
+++ src/components/Select.js
@@ -1,9 +1,8 @@
 import { useState, useEffect, useRef } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import scrollIntoView from "scroll-into-view-if-needed";
 
 export default function Select(props) {
-
     const [uniqueID, setUniqueID] = useState();
     const [dropDownOn, setDropDownOn] = useState(false);
     const [search, setSearch] = useState("");
@@ -13,7 +12,6 @@ export default function Select(props) {
     const [highlightedOption, setHighlightedOption] = useState("");
 
     const selectRef = useRef(null);
-
 
     const getSearchResults = () => {
         let searchResults = {};
@@ -25,52 +23,51 @@ export default function Select(props) {
         }
 
         return searchResults;
-    }
+    };
 
     const getPlaceholder = () => {
         if (props.withCheck) {
             if (!selected.length) return props.placeholder;
             if (props.withCheck === "single") return options[selected];
 
-            return selected.map(value => props.options[value]).join(", ");
+            return selected.map((value) => props.options[value]).join(", ");
         } else {
             if (!selected) return props.placeholder;
 
             return options[selected];
         }
-    }
-
-
+    };
 
     const onClick = (value) => {
         setSelected(value);
         setDropDownOn(false);
 
         props.onChange(value);
-    }
+    };
 
     const onChange = (value) => {
         if (props.withCheck === "single") {
+            selectRef.current.querySelectorAll(".select__button")[0].focus();
             setSelected(selected === value ? "" : value);
             props.onChange(value);
-        }
-        if (props.withCheck === "multiple") {
+        } else if (props.withCheck === "multiple") {
             const newSelected = [...selected];
+            selectRef.current.querySelectorAll(".select__search")[0].focus();
             if (newSelected.includes(value)) {
                 newSelected.splice(newSelected.indexOf(value), 1);
             } else {
-                newSelected.push(value)
+                newSelected.push(value);
             }
             setSelected(newSelected);
             props.onChange(newSelected);
         }
-    }
+    };
 
     const onBlur = (e) => {
         if (e.relatedTarget && !selectRef.current.contains(e.relatedTarget)) {
             setDropDownOn(false);
         }
-    }
+    };
 
     const onKeyDown = (e) => {
         const keyCode = e.keyCode;
@@ -110,20 +107,24 @@ export default function Select(props) {
 
             setHighlightedOption(value);
 
-            scrollIntoView(selectRef.current.querySelector(`.${props.classNamePrefix}__menu [data-value=${value}]`), {
-                scrollMode: "if-needed",
-                block: "nearest",
-                inline: "nearest",
-            })
+            scrollIntoView(
+                selectRef.current.querySelector(
+                    `.${props.classNamePrefix}__menu [data-value=${value}]`
+                ),
+                {
+                    scrollMode: "if-needed",
+                    block: "nearest",
+                    inline: "nearest",
+                }
+            );
         }
-    }
+    };
 
     const onClickOutside = (event) => {
         if (selectRef.current && !selectRef.current.contains(event.target)) {
             setDropDownOn(false);
         }
-    }
-
+    };
 
     useEffect(() => {
         setUniqueID(Math.random().toString(16).slice(2));
@@ -139,7 +140,9 @@ export default function Select(props) {
     }, [props.value]);
 
     useEffect(() => {
-        setOptions(props.withCheck === "multiple" ? getSearchResults() : props.options);
+        setOptions(
+            props.withCheck === "multiple" ? getSearchResults() : props.options
+        );
     }, [props.options]);
 
     useEffect(() => {
@@ -154,10 +157,13 @@ export default function Select(props) {
         setOptions(getSearchResults());
     }, [search]);
 
-
     const generateOptions = () => {
         if (!Object.keys(options).length) {
-            return <div className={`${props.classNamePrefix}__no-res`}>List is Empty</div>
+            return (
+                <div className={`${props.classNamePrefix}__no-res`}>
+                    List is Empty
+                </div>
+            );
         }
 
         if (props.withCheck) {
@@ -165,105 +171,156 @@ export default function Select(props) {
                 const label = options[value];
                 const labelID = `${uniqueID}__${value}`;
 
-                return <label
-                    key={index}
-                    data-value={value}
-                    className={`${props.classNamePrefix}__menu-item${value === highlightedOption ? ` ${props.classNamePrefix}__menu-item--highlighted` : ""}`}
-                    onMouseEnter={() => setHighlightedOption(value)}
-                    htmlFor={labelID}
-                >
-                    <input
-                        id={labelID}
-                        tabIndex="-1"
-                        type="checkbox"
-                        checked={props.withCheck === "single" ? selected === value : selected.includes(value)}
-                        onChange={() => onChange(value)}
-                    />
-                    {props.drawOption ? props.drawOption(value, label) : <div>{label}</div>}
-                </label>
-            })
+                return (
+                    <label
+                        key={index}
+                        data-value={value}
+                        className={`${props.classNamePrefix}__menu-item${
+                            value === highlightedOption
+                                ? ` ${props.classNamePrefix}__menu-item--highlighted`
+                                : ""
+                        }`}
+                        onMouseEnter={() => setHighlightedOption(value)}
+                        htmlFor={labelID}
+                    >
+                        <input
+                            id={labelID}
+                            tabIndex="-1"
+                            type="checkbox"
+                            checked={
+                                props.withCheck === "single"
+                                    ? selected === value
+                                    : selected.includes(value)
+                            }
+                            onChange={() => onChange(value)}
+                        />
+                        {props.drawOption ? (
+                            props.drawOption(value, label)
+                        ) : (
+                            <div>{label}</div>
+                        )}
+                    </label>
+                );
+            });
         } else {
             return Object.keys(options).map((value, index) => {
                 const label = options[value];
 
-                return <div
-                    key={index}
-                    data-value={value}
-                    className={`${props.classNamePrefix}__menu-item${value === highlightedOption ? ` ${props.classNamePrefix}__menu-item--highlighted` : ""}${value === selected ? ` ${props.classNamePrefix}__menu-item--selected` : ""}`}
-                    onMouseEnter={() => setHighlightedOption(value)}
-                    onClick={() => onClick(value)}
-                >
-                    {props.drawOption ? props.drawOption(value, label) : <div>{label}</div>}
-                </div>
-            })
+                return (
+                    <div
+                        key={index}
+                        data-value={value}
+                        className={`${props.classNamePrefix}__menu-item${
+                            value === highlightedOption
+                                ? ` ${props.classNamePrefix}__menu-item--highlighted`
+                                : ""
+                        }${
+                            value === selected
+                                ? ` ${props.classNamePrefix}__menu-item--selected`
+                                : ""
+                        }`}
+                        onMouseEnter={() => setHighlightedOption(value)}
+                        onClick={() => onClick(value)}
+                    >
+                        {props.drawOption ? (
+                            props.drawOption(value, label)
+                        ) : (
+                            <div>{label}</div>
+                        )}
+                    </div>
+                );
+            });
         }
-    }
+    };
 
     const selectButton = () => {
-        return <div
-            className={`${props.classNamePrefix}__button`}
-            tabIndex="0"
-            onMouseUp={() => setDropDownOn(!dropDownOn)}
-            onKeyDown={e => {
-                e.stopPropagation();
+        return (
+            <div
+                className={`${props.classNamePrefix}__button`}
+                tabIndex="0"
+                onMouseUp={() => setDropDownOn(!dropDownOn)}
+                onKeyDown={(e) => {
+                    e.stopPropagation();
 
-                if (dropDownOn) {
-                    if (e.keyCode === 38 || e.keyCode === 40) e.preventDefault();
+                    if (dropDownOn) {
+                        if (e.keyCode === 38 || e.keyCode === 40)
+                            e.preventDefault();
 
-                    onKeyDown(e);
-                }
+                        onKeyDown(e);
+                    }
 
-                if (!(props.withCheck && dropDownOn)) {
-                    e.keyCode === 13 && setDropDownOn(!dropDownOn)
-                }
-            }}
-            onBlur={onBlur}
-        >
-            <div className={`${props.classNamePrefix}__value`}>
-                <span>{placeholder}</span>
+                    if (!(props.withCheck && dropDownOn)) {
+                        e.keyCode === 13 && setDropDownOn(!dropDownOn);
+                    }
+                }}
+                onBlur={onBlur}
+            >
+                <div className={`${props.classNamePrefix}__value`}>
+                    <span>{placeholder}</span>
+                </div>
+                <div className={`${props.classNamePrefix}__arrow`}>
+                    <span
+                        className={`${props.classNamePrefix}__icon ${props.classNamePrefix}__icon--arrow-down`}
+                    ></span>
+                </div>
             </div>
-            <div className={`${props.classNamePrefix}__arrow`}>
-                <span className={`${props.classNamePrefix}__icon ${props.classNamePrefix}__icon--arrow-down`}></span>
-            </div>
-        </div>
-    }
+        );
+    };
 
     const selectSearch = () => {
-        return <div className={`${props.classNamePrefix}__button`}>
-            <input
-                type="text"
-                className={`${props.classNamePrefix}__search`}
-                autoFocus
-                value={search}
-                placeholder="Search..."
-                onChange={e => setSearch(e.target.value)}
-                onKeyDown={e => { if (e.keyCode === 13 && !highlightedOption) e.stopPropagation() }}
-                onFocus={() => setDropDownOn(true)}
-                onBlur={onBlur}
-            />
-            <div className={`${props.classNamePrefix}__arrow`}>
-                {
-                    props.withCheck === "multiple" &&
+        return (
+            <div className={`${props.classNamePrefix}__button`}>
+                <input
+                    type="text"
+                    className={`${props.classNamePrefix}__search`}
+                    autoFocus
+                    value={search}
+                    placeholder="Search..."
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.keyCode === 13 && !highlightedOption)
+                            e.stopPropagation();
+                    }}
+                    onFocus={() => setDropDownOn(true)}
+                    onBlur={onBlur}
+                />
+                <div className={`${props.classNamePrefix}__arrow`}>
+                    {props.withCheck === "multiple" && (
+                        <button
+                            className={`${props.classNamePrefix}__btn`}
+                            onClick={() => setSelected([])}
+                            onKeyDown={(e) => {
+                                if (e.keyCode === 13) {
+                                    e.stopPropagation();
+                                    setSelected([]);
+                                }
+                            }}
+                            onBlur={onBlur}
+                        >
+                            <span
+                                className={`${props.classNamePrefix}__icon ${props.classNamePrefix}__icon--clear`}
+                            ></span>
+                        </button>
+                    )}
                     <button
                         className={`${props.classNamePrefix}__btn`}
-                        onClick={() => setSelected([])}
-                        onKeyDown={e => { if (e.keyCode === 13) { e.stopPropagation(); setSelected([]) } }}
+                        onClick={() => setDropDownOn(false)}
+                        onKeyDown={(e) => {
+                            if (e.keyCode === 13) {
+                                e.stopPropagation();
+                                setDropDownOn(false);
+                            }
+                        }}
                         onBlur={onBlur}
                     >
-                        <span className={`${props.classNamePrefix}__icon ${props.classNamePrefix}__icon--clear`}></span>
+                        <span
+                            className={`${props.classNamePrefix}__icon ${props.classNamePrefix}__icon--x`}
+                        ></span>
                     </button>
-                }
-                <button
-                    className={`${props.classNamePrefix}__btn`}
-                    onClick={() => setDropDownOn(false)}
-                    onKeyDown={e => { if (e.keyCode === 13) { e.stopPropagation(); setDropDownOn(false) } }}
-                    onBlur={onBlur}
-                >
-                    <span className={`${props.classNamePrefix}__icon ${props.classNamePrefix}__icon--x`}></span>
-                </button>
+                </div>
             </div>
-        </div>
-    }
+        );
+    };
 
     return (
         <div
@@ -271,15 +328,18 @@ export default function Select(props) {
             onKeyDown={onKeyDown}
             className={`${props.classNamePrefix} ${props.className}`}
         >
-            {dropDownOn ?
+            {dropDownOn ? (
                 <>
                     {props.withSearch ? selectSearch() : selectButton()}
-                    <div className={`${props.classNamePrefix}__menu`}>{generateOptions()}</div>
+                    <div className={`${props.classNamePrefix}__menu`}>
+                        {generateOptions()}
+                    </div>
                 </>
-                : selectButton()
-            }
+            ) : (
+                selectButton()
+            )}
         </div>
-    )
+    );
 }
 
 Select.propTypes = {
@@ -290,11 +350,11 @@ Select.propTypes = {
     placeholder: PropTypes.string,
     className: PropTypes.string,
     classNamePrefix: PropTypes.string,
-    withCheck: PropTypes.oneOf(['multiple', 'single'])
-}
+    withCheck: PropTypes.oneOf(["multiple", "single"]),
+};
 
 Select.defaultProps = {
     placeholder: "",
     className: "",
-    classNamePrefix: "select"
-}
+    classNamePrefix: "select",
+};
